63224e7ac97c7937d43950bd20ece3f225dd9111
kvn247 <kvn24.7@live.com.au>
2016-09-19 17:45:59 +1000

    PDC
    Added PDC to control data transfer between ADC and memory

--- src/main.cpp
+++ src/main.cpp
@@ -7,69 +7,37 @@ Recorded sound is stored in PCM format at 22.05kHz 8bit
 */
 
 #include "Arduino.h"
-<<<<<<< HEAD
+
 #include <adc.h>
 
 #define SIZEOFBUFFER 1000
 
 uint16_t buffer[SIZEOFBUFFER];
 uint16_t toggle = 1;
-=======
->>>>>>> parent of 196c4e4... ADC
 
 void startTimer(){
   pmc_set_writeprotect(0); //enable the write protect register of the pmc_set_writeprotect
   pmc_enable_periph_clk(ID_TC0); //enable timer counter 0, channel 0
-
   //Configure TC0 channel 1, Waveform Mode, Reset tc_val on RC compare, chose mclk/2, TIOA toggle on RC compare
   TC_Configure(TC0, 0, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_ACPA_NONE |  TC_CMR_ACPC_TOGGLE);
   TC_SetRC(TC0, 0, 952); // sets interrupt rate; curretly set for 22.05KHz
   TC_Start(TC0, 0);       // Start the timer
-
-  // enable timer interrupts on the timer
-  // TC0->TC_CHANNEL[0].TC_IER=TC_IER_CPCS;   // IER = interrupt enable register
-  // TC0->TC_CHANNEL[0].TC_IDR=~TC_IER_CPCS;  // IDR = interrupt disable register
-
-  NVIC_EnableIRQ(TC0_IRQn);               // Enable interrupt routine
-
 }
-
-<<<<<<< HEAD
-// void TC0_Handler()
-// {
-//   //was used for testing only; this handler is currently not being used
-//   // We need to get the status to clear it and allow the interrupt to fire again
-//   TC_GetStatus(TC0, 0);
-//
-// }
-=======
-void TC0_Handler()
-{
-  // We need to get the status to clear it and allow the interrupt to fire again
-  TC_GetStatus(TC0, 0);
-
-}
->>>>>>> parent of 196c4e4... ADC
 
 void pinInit(){
   //We want to output the counter timer onto pin 2 of the Arduino
   //tc0 outputs to TIOA0, which is on peripheral B; we need to set this up
-
   PIOB->PIO_PDR = PIO_PB25B_TIOA0 ;  // disable PIO control
   PIOB->PIO_IDR = PIO_PB25B_TIOA0 ;   // disable PIO interrupts
   PIOB->PIO_ABSR |= PIO_PB25B_TIOA0 ;  // switch to B peripheral
 }
 
-<<<<<<< HEAD
 void adcSetUp(){
   pmc_enable_periph_clk(ID_ADC);
   adc_init(ADC, SystemCoreClock, ADC_FREQ_MAX, ADC_STARTUP_FAST);
 
-  // NVIC_EnableIRQ(ADC_IRQn); //enable interrupt
-  //ADC->ADC_IER = ADC_IER_EOC7; //enable end of coversion interrupt on channel 7
-  // ADC->ADC_IDR = ~(ADC_IER_EOC7);
   ADC->ADC_CHER = ADC_CHER_CH7; //enable channel 7, which is A0 in the due board
-//  ADC->ADC_CHDR = ~(ADC_CHER_CH7);
+
   ADC->ADC_MR |= ADC_MR_TRGEN_EN | ADC_MR_TRGSEL_ADC_TRIG1; // enable TIOA trigger
   ADC->ADC_MR |= ADC_MR_FREERUN_OFF;  //turn on freerun mode
   // setting up the timings for the ADC (I HAVE NO CLUE HERE, JUST CHUCKING IN RANDOM NUMBERS!!!)
@@ -81,35 +49,29 @@ void pdcSetUp(){
   //set up pointer to buffer and counter for buffer.
   PDC_ADC->PERIPH_RPR = (uint32_t)buffer;
   PDC_ADC->PERIPH_RCR = SIZEOFBUFFER;
+  PDC_ADC->PERIPH_PTCR = PERIPH_PTCR_RXTEN;
 }
 
-// void ADC_Handler(){
-//   if (ADC->ADC_ISR & ADC_ISR_DRDY){
-//     int val = (ADC->ADC_LCDR);
-//     toggle ^= 1;
-//     if(toggle)
-//     digitalWrite(10, HIGH);
-//     else
-//     digitalWrite(10, LOW);
-//   }
-// }
-
-
-
-=======
->>>>>>> parent of 196c4e4... ADC
 void setup() {
   Serial1.begin(9600); //used for debugging only
   startTimer();
   pinInit();
-<<<<<<< HEAD
   pdcSetUp();
   adcSetUp();
   pinMode(10, OUTPUT);
-=======
->>>>>>> parent of 196c4e4... ADC
 }
 
 void loop() {
-  Serial1.println("Hello World");
+
+  if((ADC->ADC_ISR & ADC_ISR_ENDRX) != 0)
+  {
+    toggle ^= 1;
+    pdcSetUp();
+  }
+
+  if(toggle)
+  digitalWrite(10, HIGH);
+  else
+  digitalWrite(10, LOW);
+
 }
